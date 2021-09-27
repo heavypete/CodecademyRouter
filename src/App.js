76cbd67dc702f47464a5eeed3c5184b375021ef0
heavypete <70510263+heavypete@users.noreply.github.com>
2021-09-27 22:04:12 +0200

    element type error

--- src/App.js
+++ src/App.js
@@ -1,11 +1,8 @@
 import "./App.scss";
+import RouterComponent from "./Components/RouterComponent/RouterComponent";
 
 function App() {
-  return (
-    <div className="App">
-      <RouterComponent />
-    </div>
-  );
+  return <div className="App">{<RouterComponent />}</div>;
 }
 
 export default App;
---
+++ src/Components/pages/Users/Users.jsx
@@ -0,0 +1,13 @@
+// import React from 'react';
+
+// const Users = () => {
+
+//     return (
+//         <div className="Users">
+//             Tested Users
+//         </div>
+//     );
+// }
+
+
+// export { Users };
